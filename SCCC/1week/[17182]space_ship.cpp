b152a9c99288cafbfff5dd95fc552c81327c9d02
Sumin0916 <gsmin2018@gmail.com>
2023-03-16 23:54:32 +0900

    Add PS

--- SCCC/1week/[17182]space_ship.cpp
+++ SCCC/1week/[17182]space_ship.cpp
@@ -2,9 +2,23 @@
 #include <memory>
 
 using namespace std;
+bool visited[11] = {false, };
+int res = 100001;
+int n, k, graph[11][11];
+
+void DFS(int start, int depth, int weight){
+    if (depth == n){res = min(res, weight);}
+    for (int i = 0; i < n; i++){
+        if (start == i){continue;}
+        if (!visited[i]){
+            visited[i] = true;
+            DFS(i, depth+1, weight+graph[start][i]);
+            visited[i] = false;    
+        }
+    }
+}
 
 int main(void){
-    int n, k, graph[11][11];
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>k;
@@ -13,12 +27,15 @@ int main(void){
             cin>>graph[i][j];
         }
     }
-    for (int k = 0; k < n; k++){
+    for (int m = 0; m < n; m++){
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+                graph[i][j] = min(graph[i][j], graph[i][m]+graph[m][j]);
             }
         }
     }
+    visited[k] = true;
+    DFS(k, 1, 0);
+    cout<<res;
     return 0;
 }
