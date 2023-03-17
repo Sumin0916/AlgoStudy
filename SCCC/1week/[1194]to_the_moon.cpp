bc8f98458b44f7398b65fcdd51f93c976a62e2bb
Sumin0916 <gsmin2018@gmail.com>
2023-03-18 00:43:30 +0900

    Edit PS

--- SCCC/1week/[1194]to_the_moon.cpp
+++ SCCC/1week/[1194]to_the_moon.cpp
@@ -2,31 +2,38 @@
 #include <algorithm>
 
 using namespace std;
-int INF = 1000000000;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int n, m;
-int dp[51][51][1<<6];
+bool dp[51][51][1<<6] = {false, };
 char map[51][51];
+int res = 100000000;
 pair<int, int> minsik;
 
 void BFS(int row, int col, int depth, int key){
     char now_stat = map[row][col];
-    if (now_stat == 1){
-        return; 
+    cout<<row<<col<<key<<depth<<endl;
+    if (now_stat == '1'){
+        cout<<"im depth:"<<depth<<endl;
+        res = min(res, depth);
+        return;
     }
-    if ('a' <= now_stat && now_stat <= 'f'){
-        int key_num = now_stat % 'a';
+    else if ('a' <= now_stat && now_stat <= 'f'){
+        int key_num = int(now_stat - 'a');
         key |= (1<<key_num); 
-        if (dp[row][col][key] < depth){return;}
-        dp[row][col][key] = min(dp[row][col][key], depth);
+    }
+    else if ('A' <= now_stat && now_stat <= 'F'){
+        int key_num = int(now_stat - 'A');
+        if (int(key&(1<<key_num))==0){return;}
     }
     int next_row, next_col;
     for (int i = 0; i < 4; i++){
         next_row = row + dx[i];
         next_col = col + dy[i];
         if ((-1 < next_row && next_row < n)&&(-1 < next_col && next_col < m)){
-            if (map[next_row][next_col]=='')
+            if (map[next_row][next_col]=='#'){continue;}
+            if (dp[next_row][next_col][key]){continue;}
+            dp[next_row][next_col][key] = true;
             BFS(next_row, next_col, depth+1, key);
         }
     }
@@ -36,17 +43,17 @@ void BFS(int row, int col, int depth, int key){
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    string input_data;
     cin>>n>>m;
-    char input_data;
-    fill(dp[0][0], dp[51][51], INF);
     for (int i = 0; i < n; i++){
+        cin>>input_data;
         for (int j = 0; j < m; j++){
-            cin>>input_data;
-            if (input_data==0){minsik.first=i;minsik.second=j;}
-            map[i][j] = input_data;
+            if (input_data[j]=='0'){minsik.first=i;minsik.second=j;}
+            map[i][j] = input_data[j];
         }
     }
-
-
+    BFS(minsik.first, minsik.second, 0, 0);
+    if (res==100000000){cout<<-1;}
+    else{cout<<res;}
     return 0;
 }
