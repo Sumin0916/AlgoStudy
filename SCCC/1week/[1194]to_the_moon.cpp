644cfa54ddb1308c915c46f384eaed5d24319ae4
Sumin0916 <gsmin2018@gmail.com>
2023-03-21 00:18:23 +0900

    Add PS

--- SCCC/1week/[1194]to_the_moon.cpp
+++ SCCC/1week/[1194]to_the_moon.cpp
@@ -2,39 +2,38 @@
 #include <algorithm>
 
 using namespace std;
+int INF =100000000;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int n, m;
-bool dp[51][51][1<<6] = {false, };
+int dp[51][51][1<<6];
 char map[51][51];
-int res = 100000000;
+int res = INF;
 pair<int, int> minsik;
 
 void BFS(int row, int col, int depth, int key){
-    char now_stat = map[row][col];
-    cout<<row<<col<<key<<depth<<endl;
-    if (now_stat == '1'){
-        cout<<"im depth:"<<depth<<endl;
-        res = min(res, depth);
-        return;
-    }
-    else if ('a' <= now_stat && now_stat <= 'f'){
-        int key_num = int(now_stat - 'a');
-        key |= (1<<key_num); 
-    }
-    else if ('A' <= now_stat && now_stat <= 'F'){
-        int key_num = int(now_stat - 'A');
-        if (int(key&(1<<key_num))==0){return;}
-    }
+    char next_stat;
     int next_row, next_col;
+    dp[row][col][key] = depth;
     for (int i = 0; i < 4; i++){
+        int next_key = key;
         next_row = row + dx[i];
         next_col = col + dy[i];
+        next_stat = map[next_row][next_col];
         if ((-1 < next_row && next_row < n)&&(-1 < next_col && next_col < m)){
-            if (map[next_row][next_col]=='#'){continue;}
-            if (dp[next_row][next_col][key]){continue;}
-            dp[next_row][next_col][key] = true;
-            BFS(next_row, next_col, depth+1, key);
+            if (next_stat =='#'){continue;}
+            else if ('a' <= next_stat && next_stat <= 'f'){
+                next_key |= (1<<int(next_stat - 'a'));
+            }
+            if (next_stat == '1'){
+                res = min(res, depth+1);
+                return;
+                }
+            else if ('A' <= next_stat && next_stat <= 'F'){
+                if ((key&(1<<int(next_stat)- 'A'))==0){continue;}
+                }
+            if (dp[next_row][next_col][key] <= depth+1){continue;}
+            BFS(next_row, next_col, depth+1, next_key);
         }
     }
     return;
@@ -45,6 +44,7 @@ int main(void){
     cin.tie(0);
     string input_data;
     cin>>n>>m;
+    fill(&dp[0][0][0], &dp[50][50][1<<6], INF);
     for (int i = 0; i < n; i++){
         cin>>input_data;
         for (int j = 0; j < m; j++){
@@ -53,7 +53,7 @@ int main(void){
         }
     }
     BFS(minsik.first, minsik.second, 0, 0);
-    if (res==100000000){cout<<-1;}
+    if (res==INF){cout<<-1;}
     else{cout<<res;}
     return 0;
 }
---
+++ SCCC/1week/[23324]HSP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int parent[100001] = {0, };
+
+int find_parent(int a, int b){
+
+}
+
+int main(void){
+    int n, m, k;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m>>k;
+
+    return 0;
+}
