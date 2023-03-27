95bc8b350f7cf02244d8c6707ce09fad1d6e13c2
Sumin0916 <gsmin2018@gmail.com>
2023-03-27 22:53:25 +0900

    Edit PS

--- SCCC/2week/[21348]video_clips.cpp
+++ SCCC/2week/[21348]video_clips.cpp
@@ -2,40 +2,28 @@
 #include<algorithm>
 #define MAX_LOG_M 31
 #define MAX_K 100001
-int global_m;
 using namespace std;
+int global_m;
 int sparse_table[MAX_LOG_M][MAX_K];
 
 void videos(int K, int M, int S[]) {
     global_m = M;
     for (int i = 0; i < K; i++){sparse_table[0][i]=S[i];}
     for (int i = 1; i < MAX_LOG_M; i++){
+        int tmp;
         for (int j = 0; j < K; j++){
-            sparse_table[i][j] = sparse_table[i-1][sparse_table[i-1][j]];
+            tmp = sparse_table[i-1][j];
+            sparse_table[i][j] = sparse_table[i-1][tmp];
         }
     }
 }
-
 
 int clip(int I){
+    int curr = sparse_table[0][I];
     for (int j = MAX_LOG_M; 0 <= j; --j){
-        if ((global_m-1) & (1<<j)){
-            I = sparse_table[j][I];
+        if ((global_m-2) & (1<<j)){
+            curr = sparse_table[j][curr];
         }
     }
-    return I;
-}
-
-int main(void){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int m,k,s[100000];
-    cin>>k>>m;
-    for (int i = 0; i < 100001; i++){
-        int tmp;
-        cin>>tmp;
-        s[i] = tmp;
-    }
-    videos(4, 2, s);
-    cout<<clip(3)<<clip(1);
-    return 0;
+    return curr;
 }
