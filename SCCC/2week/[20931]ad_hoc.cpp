8a6cf7caa8004d1981eeaef67d3c4cae772df7b4
Sumin0916 <gsmin2018@gmail.com>
2023-03-28 20:55:10 +0900

    Add files

---
+++ SCCC/2week/[20931]ad_hoc.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bitset>
+#define MAX_LOG_L 61
+#define MAX_QUERY 150001
+using namespace std;
+
+int sparse_table[MAX_LOG_L][MAX_QUERY];
+
+int main(void){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    bitset<61> bitl;
+    int Q;
+    bitl.reset();
+    cin>>Q;
+
+    return 0;
+}
