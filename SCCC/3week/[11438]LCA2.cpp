c5eac35c13986ba665a31fcbb559bafc78815841
Sumin0916 <gsmin2018@gmail.com>
2023-04-02 02:43:08 +0900

    Add LCA2

--- SCCC/3week/[11438]LCA2.cpp
+++ SCCC/3week/[11438]LCA2.cpp
@@ -1,70 +1,67 @@
 #include<iostream>
 #include<vector>
-#define MAX_LOG_N 19
+#include<memory.h>
+
+#define MAX_LOG_N 18
 #define MAX_N 100001
 using namespace std;
 int sparse_table[MAX_LOG_N][MAX_N];
-int level[MAX_N] = {0, };
+int level[MAX_N];
 vector<int> tree[MAX_N];
 
 
-void dfs(int curr, int parent){
-    level[curr] = level[parent]+1;
-    sparse_table[0][curr] = parent;
-    for (auto n : tree[curr]){
-        dfs(n, curr);
-    }
-}
-
-int go(int start, int num){
-    int curr = sparse_table[0][start];
-    for (int i = MAX_LOG_N; i < 0; --i){
-            if ((num-1) & (1<<i)){
-                curr = sparse_table[i][curr];
-            }        
+void dfs(int curr){
+    for (int n : tree[curr]){
+        if(level[n] == -1){
+            sparse_table[0][n] = curr;
+            level[n] = level[curr] + 1;
+            dfs(n);
         }
-    return curr;
+    }
 }
 
 int main(void){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int N, M;
     cin>>N;
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N-1; i++){
         int s, e;
         cin>>s>>e;
+        s--;e--;
         tree[s].push_back(e);
         tree[e].push_back(s);
     }
-    dfs(1, 0);
-    for (int i = 1; i < MAX_LOG_N; i++){
-        for (int j = 1; j <= MAX_N; j++){
-            int temp = sparse_table[i-1][j];
-            sparse_table[i][j] = sparse_table[i-1][temp];
+    memset(sparse_table, -1, sizeof(sparse_table));
+    fill(level, level+N, -1);
+    level[0] = 0;
+    dfs(0);
+    for (int i = 0; i < MAX_LOG_N-1; i++){
+        for (int j = 1; j < N; j++){
+            if (sparse_table[i][j] != -1)
+            sparse_table[i+1][j] = sparse_table[i][sparse_table[i][j]];
         }
     }
     cin>>M;
     for (int i = 0; i < M; i++){
-        int s, e, res;
+        int s, e;
         cin>>s>>e;
-        int diff = abs(level[s]-level[e]);
-        if (level[s] > level[e]){
-            for (int j = 0; j < level[e]; j++){
-                int sp = go(s, diff+j);
-                int ep = go(e, j);
-                if (sp == ep){cout<<sp;continue;}
+        s--;e--;
+        if (level[s] < level[e]){swap(s, e);}
+        int diff = level[s] - level[e];
+        for (int j = 0; diff; j++){
+            if (diff%2) s = sparse_table[j][s];
+            diff /= 2;
+        }
+        if (s != e){
+            for (int j = MAX_LOG_N-1; j >= 0; --j){
+                if (sparse_table[j][s] != -1 && sparse_table[j][e] != sparse_table[j][s]){
+                    s = sparse_table[j][s];
+                    e = sparse_table[j][e];
+                }
             }
+            s = sparse_table[0][s];
         }
-        else if (level[s] < level[e]){
-            for (int i = 0; i < level[s]; i++){
-                int sp = go(s, i);
-                int ep = go(e, diff+i);
-                if (sp == ep){cout<<sp;continue;}
-            }
-        }
-        else{
-            cout<<sparse_table[0][s];
-        }
+        cout<<s+1<<'\n';
     }
     return 0;
 }
