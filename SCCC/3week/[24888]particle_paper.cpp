51ed03b5d2fed93bfb661edd559c7e16633fb89e
Sumin0916 <gsmin2018@gmail.com>
2023-04-09 00:27:56 +0900

    Add unsolved files....

--- SCCC/3week/[24888]particle_paper.cpp
+++ SCCC/3week/[24888]particle_paper.cpp
@@ -8,9 +8,32 @@ using namespace std;
 const int INF = 987654321;
 const int MAX_N = 200001;
 typedef pair<int, int> p;
+long long dist[MAX_N];
 int note_num = 0;
 vector<p> adj[MAX_N];
 vector<int> res;
+vector<int> n_count(MAX_N, 0);
+int note[MAX_N];
+
+void res_bt(int node, int pre){
+    if (node == 1){
+        cout<<note_num<<'\n';
+        for (int i = res.size()-1; i > -1; i--){
+        cout<<res[i]<<" ";
+    }
+        exit(0);
+    }
+
+    for (p &a : adj[node]){
+        int n = a.first; int d = a.second;
+        if (pre == n){continue;}
+        if (dist[node] != dist[n]+d){continue;}
+        if (n_count[node] != n_count[n]+note[node]){continue;}
+        res.push_back(n);
+        res_bt(n, node);
+        res.pop_back();
+    }
+}
 
 int main(void){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -22,57 +45,34 @@ int main(void){
         adj[s].push_back(make_pair(e, w));
         adj[e].push_back(make_pair(s, w));
     }
-    int dist[MAX_N];
-    int prev[MAX_N];
-    int visited[MAX_N] = {0, };
-    int note[MAX_N];
-    fill(prev, prev+MAX_N, -1);
     fill(dist, dist+MAX_N, INF);
     dist[1] = 0;
-    priority_queue<p, vector<p>, greater<p>> PQ;
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> PQ;
     PQ.push(p(0, 1));
+    for (int i = 1; i < N+1; i++){
+        cin>>note[i];
+        note_num += note[i];
+    }
+    n_count[1] = note[1];
     while (!PQ.empty()){
-        int curr;
-        do{
-            curr = PQ.top().second;
-            PQ.pop();
-        }while(!PQ.empty() && visited[curr]);
-        if(visited[curr]){break;}
-        visited[curr] = true;
-        for (auto &a:adj[curr]){
+        int curr = PQ.top().second;
+        long long cost = PQ.top().first;
+        PQ.pop();
+        if (dist[curr]< cost){continue;}
+        for (p &a:adj[curr]){
             int next = a.first;
-            int dis = a.second;
-            if (dist[next] > dist[curr]+dis){
-                dist[next] = dist[curr]+dis;
-                prev[next] = curr;
+            long long next_cost = cost + a.second;
+            if (dist[next] > next_cost){
+                n_count[next] = max(n_count[next], n_count[curr]+note[next]);
+                dist[next] = next_cost;
                 PQ.push(p(dist[next], next));
             }
+            else if (dist[next] == next_cost){n_count[next] = max(n_count[next], n_count[curr]+note[next]);}
         }
     }
-    for (int i = 1; i < N+1; i++){
-        int n;
-        cin>>n;
-        if(n==1){note_num++;}
-        note[i] = n;
-    }
-    int root = N;
-    while(root != -1){
-        res.push_back(root);
-        root = prev[root];
-    }
-    reverse(res.begin(),res.end());
-    for (int i = 0; i < res.size(); i++){
-        if (note[res[i]] == 1){
-            note[res[i]] = 0;
-            note_num--;
-        }
-    }
-    if (note_num == 0){
-        cout<<res.size()<<'\n';
-        for (auto i: res){
-            cout<<i<<' ';
-        }
-    }
-    else{cout<<-1;}
+
+    if (note_num != n_count[N]){cout<<-1;return 0;}
+    res.push_back(N);
+    res_bt(N, 0);
     return 0;
 }
