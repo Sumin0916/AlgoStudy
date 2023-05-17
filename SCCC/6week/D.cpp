2c91dafc7112cde8794ae19e580f7f4c442f0320
Sumin0916 <gsmin2018@gmail.com>
2023-05-17 17:43:27 +0900

    Add two more PS

--- SCCC/6week/D.cpp
+++ SCCC/6week/D.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
 
+set<string> S;
+string str_lst[100002], str1, str2;
+
 int main(void){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int t;
+    int t, res;
     cin >> t;
     for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
-    }    
+        for (int j = 0; j < n; j++) {
+            cin >> str1;
+            str_lst[j] = str1;
+            S.insert(str1);
+        }
+        for (int j = 0; j < n; j++) {
+            int s_len = str_lst[j].length();
+            str1 = str_lst[j];
+            str2.clear();
+            res = 0;
+            for (int k = 0; k < s_len; k++) {
+                str1.erase(str1.begin());
+                str2.push_back(str_lst[j][k]);
+                if (S.find(str1) != S.end() && S.find(str2) != S.end()) {res = 1;break;}
+            }
+            cout << res;
+        }
+        cout << '\n';
+        S.clear();
+    }
     return 0;
 }
--- SCCC/6week/E.cpp
+++ SCCC/6week/E.cpp
@@ -7,13 +7,37 @@ using namespace std;
 int t, n;
 int array[101][101];
 
+void rotate() {
+    int temp[101][101];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            temp[j][n-i-1] = array[i][j];
+    memcpy(array, temp, sizeof(temp));
+}
+
 int main(void){
     cin >> t;
     for (int i = 0; i < t; i++) {
         int res = 0;
-        int mirror[101][101];
+        int mirror[101][101] = {0, };
         cin >> n;
         for (int j = 0; j < n; j++){for (int k = 0; k < n; k++){scanf("%1d", &array[j][k]);}}
+        for (int j = 0; j < 4; j++){
+            for (int m = 0; m < n; m++){
+                for (int k = 0; k < n; k++){
+                    if (array[m][k] == 1){mirror[m][k] += 1;}
+                    else {mirror[m][k] -= 1;}
+                }
+            }
+            rotate();
+        }
+        for (int m = 0; m < n; m++){
+            for (int k = 0; k < n; k++){
+                if ((mirror[m][k] <= 0) && (array[m][k] == 1))res++;
+                else if ((mirror[m][k] > 0) && (array[m][k] == 0))res++;
+            }
+        }
+        cout << res << endl;
     }
     return 0;
 }
