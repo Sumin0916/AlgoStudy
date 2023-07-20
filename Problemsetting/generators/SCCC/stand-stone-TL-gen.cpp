4c74a65ffd9e6ffd1962439cb8b66da4260ec96a
Sumin0916 <gsmin2018@gmail.com>
2023-07-20 23:16:05 +0900

    Edit gen

--- Problemsetting/generators/SCCC/stand-stone-TL-gen.cpp
+++ Problemsetting/generators/SCCC/stand-stone-TL-gen.cpp
@@ -11,9 +11,8 @@ int main(int argc, char *argv[]) {
     int NMIN = opt<int>("NMIN");
     int N = rnd.next(NMIN, NMAX);
     cout << N << endl;
-    A[0] = rnd.next(1, 3);
-    for(int i = 1; i < N; i++) {
-        rnd.next(1, 3);
+    for(int i = 0; i < N; i++) {
+        A[i] = rnd.next(1, 3);
     }
     for (int i=0; i<N-1;i++) cout << A[i] << ' ';
     cout << A[N-1] << endl;
